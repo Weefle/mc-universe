4d93d06fb2c0846dfd3c5afbae8525b2659bf045
Octopod <octopodsquad@gmail.com>
2014-03-02 14:48:54 -1000

    added the server the player came from as a variable

--- src/main/java/com/octopod/network/events/player/NetworkPlayerRedirectEvent.java
+++ src/main/java/com/octopod/network/events/player/NetworkPlayerRedirectEvent.java
@@ -3,20 +3,25 @@ package com.octopod.network.events.player;
 import com.octopod.network.events.Event;
 
 public class NetworkPlayerRedirectEvent extends Event {
-	
-	String player, server;
-	
-	public NetworkPlayerRedirectEvent(String player, String server) {
+
+	String player, from, server;
+
+	public NetworkPlayerRedirectEvent(String player, String from, String server) {
 		this.player = player;
+        this.from = from;
 		this.server = server;
 	}
-	
+
 	public String getPlayer() {
 		return player;
 	}
-	
+
 	public String getServer() {
 		return server;
 	}
-	
+
+    public String getFromServer() {
+        return from;
+    }
+
 }
