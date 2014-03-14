f1c6fe1a8364be0e62841a7a8bd2d8c384b36ab3
Octopod <octopodsquad@gmail.com>
2014-03-14 01:24:17 -1000

    moved LilyPadListeners into the LilyPadConnection class

--- src/main/java/com/octopod/network/connection/LilypadConnection.java
+++ src/main/java/com/octopod/network/connection/LilypadConnection.java
@@ -1,16 +1,27 @@
 package com.octopod.network.connection;
 
+import com.octopod.network.NetworkConfig;
+import com.octopod.network.NetworkPlus;
 import com.octopod.network.NetworkPlusPlugin;
-import com.octopod.network.util.RequestUtils;
+import com.octopod.network.events.EventEmitter;
+import com.octopod.network.events.network.NetworkConnectedEvent;
+import com.octopod.network.util.BukkitUtils;
+
 import lilypad.client.connect.api.Connect;
+import lilypad.client.connect.api.event.EventListener;
+import lilypad.client.connect.api.event.MessageEvent;
 import lilypad.client.connect.api.request.Request;
 import lilypad.client.connect.api.request.RequestException;
+import lilypad.client.connect.api.request.impl.GetPlayersRequest;
 import lilypad.client.connect.api.request.impl.MessageRequest;
 import lilypad.client.connect.api.request.impl.RedirectRequest;
 import lilypad.client.connect.api.result.Result;
 import lilypad.client.connect.api.result.StatusCode;
+import lilypad.client.connect.api.result.impl.GetPlayersResult;
+import lilypad.client.connect.api.result.impl.MessageResult;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,12 +31,96 @@ import java.util.List;
 public class LilypadConnection extends NetworkConnection {
 
     /**
-     * The Lilypad connection instancea
+     * A nested class for Lilypad's listeners, which will just redirect to our event system.
+     */
+    private static class Listener
+    {
+        @EventListener
+        public void messageReceived(MessageEvent event) {
+
+            String channel = event.getChannel(), username, message;
+
+            if(!channel.startsWith(NetworkConfig.getRequestPrefix())) {
+                return;
+            }
+
+            try {
+                username = event.getSender();
+                message = event.getMessageAsString();
+            } catch (UnsupportedEncodingException e) {
+                return;
+            }
+
+            EventEmitter.getEmitter().triggerEvent(new com.octopod.network.events.relays.MessageEvent(username, channel, message));
+
+        }
+    }
+
+    /**
+     * The current instance of Lilypad listeners.
+     */
+    private Listener listener;
+
+    /**
+     * The Lilypad connection instance.
      */
     private Connect connection;
 
-    public LilypadConnection(NetworkPlusPlugin plugin) {
+    /**
+     * Gets the Connect instance and ensures that it is connected.
+     * This should fire the NetworkConnectedEvent after it connects.
+     * @param plugin
+     */
+    public LilypadConnection(final NetworkPlusPlugin plugin) {
+
         connection = plugin.getServer().getServicesManager().getRegistration(Connect.class).getProvider();
+
+        //Unregisters any listeners if they exist.
+        if(listener != null) {
+            connection.unregisterEvents(listener);
+        }
+
+        connection.registerEvents(listener = new Listener());
+
+        if(connection != null && !connection.isConnected()) {
+
+            new Thread(new Runnable() {
+
+                @Override
+                public void run()
+                {
+                    if(!connection.isConnected()) {
+                        BukkitUtils.console("Waiting for LilyPad to connect...");
+                        int connectionAttempts = 0;
+
+                        //Waits for LilyPad to be connected
+                        while(!connection.isConnected() && connectionAttempts < 10) {
+                            try {
+                                BukkitUtils.console("Waiting for LilyPad connection... " + connectionAttempts);
+                                synchronized(this) {
+                                    wait(1000);
+                                }
+                            } catch (InterruptedException e) {}
+                            connectionAttempts++;
+                        }
+
+                        //If LilyPad still isn't connected, then most commands should be disabled.
+                        if(!connection.isConnected())
+                        {
+                            NetworkPlus.getLogger().info("&cLilypad could not connect!");
+                            plugin.disablePlugin();
+                            return;
+                        }
+                    }
+                    EventEmitter.getEmitter().triggerEvent(new NetworkConnectedEvent());
+                }
+
+            }).start();
+        } else
+        if(isConnected()) {
+            EventEmitter.getEmitter().triggerEvent(new NetworkConnectedEvent());
+        }
+
     }
 
     /**
@@ -44,20 +139,32 @@ public class LilypadConnection extends NetworkConnection {
 
     @Override
     public boolean isConnected() {
-        return connection.isConnected();
+        return (connection != null && connection.isConnected());
     }
 
     @Override
     public String getUsername() {
+        if(connection == null) return "";
         return connection.getSettings().getUsername();
     }
 
     @Override
+    public boolean serverExists(String server) {
+        return sendMessageRequest(server, "", "").getStatusCode() == StatusCode.SUCCESS;
+    }
+
     //This method will just do nothing if the request somehow fails or can't be encoded.
-    public void sendMessage(String server, String channel, String message) {
+    private MessageResult sendMessageRequest(String server, String channel, String message) {
         try {
-            sendRequest(new MessageRequest(server, channel, message));
-        } catch (UnsupportedEncodingException e) {}
+            return (MessageResult)sendRequest(new MessageRequest(server, channel, message));
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void sendMessage(String server, String channel, String message) {
+        sendMessageRequest(server, channel, message);
     }
 
     @Override
@@ -75,8 +182,18 @@ public class LilypadConnection extends NetworkConnection {
     }
 
     @Override
+    public List<String> getPlayers() {
+        GetPlayersResult result = (GetPlayersResult)sendRequest(new GetPlayersRequest(true));
+        if(result.getStatusCode() == StatusCode.SUCCESS) {
+            return new ArrayList<>(result.getPlayers());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
     public boolean sendPlayer(String player, String server) {
-        return RequestUtils.request(new RedirectRequest(server, player)).getStatusCode() == StatusCode.SUCCESS;
+        return sendRequest(new RedirectRequest(server, player)).getStatusCode() == StatusCode.SUCCESS;
     }
 
 }
