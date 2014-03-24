7f21edc54e8092a6c6b0dcd1b56de8e46059347b
Octopod <octopodsquad@gmail.com>
2014-03-24 13:53:41 -1000

    updated references to EventManager and SynchronizableEvent.

--- src/main/java/com/octopod/network/events/SynchronizedListener.java
+++ src/main/java/com/octopod/network/events/SynchronizedListener.java
@@ -1,47 +1,65 @@
 package com.octopod.network.events;
 
-public abstract class SynchronizedListener <T extends Event> {
+public class SynchronizedListener<T extends Event> implements Listener<T>, ListenerIdentifier {
 
-    private EventListener<T> listener;
+    private final Class<T> type;
+    private Listener<T> listener;
 	private final Object lock = new Object();
-	private int executions = 0;
 
-	public SynchronizedListener(EventListener<T> listener) {
-		this.listener = listener;
+	public SynchronizedListener(Class<T> type, Listener<T> listener) {
+		this.type = type;
+        this.listener = listener;
 	}
 
-	abstract public void executionListener(T event);
+    /**
+     * Implementation of ListenerIdentifier.
+     * Because the below listener is a Generic type (Event) then we will use getType()
+     * to get the Event type instead.
+     * @return
+     */
+    @Override
+    public Class<T> getType() {return type;}
 
-	public void execute(T event) {
+    /**
+     * The synchronized listener. Runs the listener provided in the constructor,
+     * and if the event is unlocked, notifies the lock.
+     * @param event
+     */
+    @Override
+    @EventHandler
+	public void onEvent(T event) {
 		try {
-			if(listener.onEvent(event)) {
-				synchronized(lock) {
-					executions++;
-					lock.notify();
-				}
-			}
+            listener.onEvent(event);
+            if(event.isUnlocked()) {
+                event.setUnlocked(false);
+                synchronized(lock) {
+                    executions++;
+                    lock.notify();
+                }
+            }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public SynchronizedListener<T> register() {
-		EventManager.getManager().registerListener(this);
-		return this;
-	}
+    private int executions = 0;
 
-	public SynchronizedListener<T> unregister() {
-		EventManager.getManager().registerListener(this);
-		return this;
-	}
-
+    /**
+     * Waits for the above listener to be fired X times, then returns.
+     * Use timeout to timeout after X ms and return anyway.
+     * @param timeout
+     * @param expectedExecutions
+     * @return
+     */
 	public SynchronizedListener<T> waitFor(long timeout, int expectedExecutions) {
 		long startTime = System.currentTimeMillis();
+        executions = 0;
 		try {
 			while(executions < expectedExecutions) {
 				if((System.currentTimeMillis() - startTime) > timeout)
 					break;
 				synchronized(lock) {
+
 					lock.wait(timeout);
 				}
 			}
@@ -50,12 +68,7 @@ public abstract class SynchronizedListener <T extends Event> {
 	}
 
 	public SynchronizedListener<T> waitFor(long timeout) {
-		try {
-			synchronized(lock) {
-				lock.wait(timeout);
-			}
-		} catch (InterruptedException e) {}
-		return this;
+		return waitFor(timeout, 1);
 	}
 
 }
