fa323bec842eb4b8522947082e1e927984df8873
Octopod <octopodsquad@gmail.com>
2014-03-24 13:55:02 -1000

    Listener's onEvent() now returns void.

--- src/main/java/com/octopod/network/events/Listener.java
+++ src/main/java/com/octopod/network/events/Listener.java
@@ -4,8 +4,8 @@ package com.octopod.network.events;
  * @author Octopod
  *         Last updated on 3/7/14
  */
-public interface EventListener<T extends Event> {
+public interface Listener<T extends Event> {
 
-    public boolean onEvent(T event);
+    public void onEvent(T event);
 
 }
