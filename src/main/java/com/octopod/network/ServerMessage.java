d9afa9465d8fd94a35b3355b131ad7bc44c062c9
Octopod <octopodsquad@gmail.com>
2014-05-09 02:34:53 -1000

    renamed NetworkServerMessage to ServerMessage, and added a comment.

--- src/main/java/com/octopod/network/ServerMessage.java
+++ src/main/java/com/octopod/network/ServerMessage.java
@@ -7,11 +7,16 @@ import java.util.Arrays;
  * @author Octopod
  *         Created on 4/14/14
  */
-public class NetworkServerMessage {
+public class ServerMessage {
 
     private ArrayList<String> arguments;
 
-    public NetworkServerMessage(String... args) {
+    /**
+     * An empty ServerMessage.
+     */
+    public static final ServerMessage EMPTY = new ServerMessage();
+
+    public ServerMessage(String... args) {
         arguments = new ArrayList<>(Arrays.asList(args));
     }
 
@@ -19,6 +24,11 @@ public class NetworkServerMessage {
         return arguments.toArray(new String[arguments.size()]);
     }
 
+    /**
+     * Returns a serialized version of the arguments.
+     * TODO: Find an easier and quicker way to serialize and deserialize just a list of Strings.
+     * @return A serialized String.
+     */
     public String toString() {
         return NetworkPlus.gson().toJson(this);
     }
