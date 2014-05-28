54b704d3f5cc56d03fe08473d30fd54418b2d477
Octopod <octopodsquad@gmail.com>
2014-05-27 16:58:57 -1000

    various NetworkLogger changes

--- src/main/java/com/octopod/network/NetworkLogger.java
+++ src/main/java/com/octopod/network/NetworkLogger.java
@@ -1,6 +1,8 @@
 package com.octopod.network;
 
 import com.octopod.network.bukkit.BukkitUtils;
+import com.octopod.octal.minecraft.ChatUtils;
+import com.octopod.octal.minecraft.ChatUtils.ChatColor;
 import org.bukkit.entity.Player;
 
 public class NetworkLogger {
@@ -9,7 +11,7 @@ public class NetworkLogger {
      * If this is true, it will show all debug messages regardless of the "debug" config option.
      * THIS SHOULDN'T BE TRUE IN RELEASE BUILDS.
      */
-    private static final boolean TESTING = true;
+    private static final boolean TESTING = false;
 
     /**
      * Sends a message at a level to the console and players with the "network.debug.<level>" permission.
@@ -22,22 +24,34 @@ public class NetworkLogger {
     {
         if(NetworkConfig.getDebugMode() >= level || TESTING)
         {
-            for(String message: messages) {
-                message = "&7" + message;
-
-                BukkitUtils.console(message);
-                for(Player player: BukkitUtils.getPlayers()) {
-                    //Check permissions going down levels.
-                    for(int i = level; i <= 0; i--) {
-                        if(player.hasPermission("network.debug." + i)) {
-                            BukkitUtils.sendMessage(player, "&7" + message);
-                            break;
-                        }
-                    }
-                }
-            }
+            send(messages);
+			for(Player player: BukkitUtils.getPlayers()) {
+				//Check permissions going down levels.
+				if(level == 0 && player.hasPermission("network.log")) {
+					send(player, messages);
+					break;
+				}
+				for(int i = level; i <= 1; i--) {
+					if(player.hasPermission("network.log." + i)) {
+						send(player, messages);
+						break;
+					}
+				}
+			}
         }
     }
+
+	private void send(String... messages) {
+		for(String message: messages) {
+			BukkitUtils.console(ChatColor.GRAY + ChatUtils.colorize(message));
+		}
+	}
+
+	private void send(Player player, String... messages) {
+		for(String message: messages) {
+			player.sendMessage(ChatColor.GRAY + ChatUtils.colorize(message));
+		}
+	}
 
     public void info(String... messages) {
         log(0, messages);
