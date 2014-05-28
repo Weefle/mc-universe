65c81eea983873ab1fe319f166fe2c9681787083
Octopod <octopodsquad@gmail.com>
2014-05-27 16:55:50 -1000

    fixed reference to ChatColor

--- src/main/java/com/octopod/network/commands/CommandHelp.java
+++ src/main/java/com/octopod/network/commands/CommandHelp.java
@@ -5,7 +5,7 @@ import com.octopod.network.CommandManager;
 import com.octopod.network.bukkit.BukkitUtils;
 import com.octopod.octal.minecraft.ChatElement;
 import com.octopod.octal.minecraft.ChatUtils;
-import com.octopod.octal.minecraft.ChatUtils.Color;
+import com.octopod.octal.minecraft.ChatUtils.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class CommandHelp extends NetworkCommand {
@@ -20,7 +20,7 @@ public class CommandHelp extends NetworkCommand {
 	public boolean exec(CommandSender sender, String label, String[] args) {
 
         BukkitUtils.sendMessage(sender, "&8-----------------------------------------------------", "");
-		BukkitUtils.sendMessage(sender, ChatUtils.toLegacy(ChatUtils.blockElement(new ChatElement("Network Commands").setColor(Color.AQUA), 320, 2)), "");
+		BukkitUtils.sendMessage(sender, ChatUtils.toLegacy(ChatUtils.blockElement(new ChatElement("Network Commands").setColor(ChatColor.AQUA), 320, 2)), "");
 		BukkitUtils.sendMessage(sender, "&8-----------------------------------------------------", "");
 
 		for(NetworkCommand command: CommandManager.getCommands().values()) {
